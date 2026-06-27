b252e772244930756f223b5b176529b744311c60
ARUN-1-tech <arunDevil2007@gmail.com>
2026-06-27 19:02:01 +0530

    Fix final...

--- attendance/api_views.py
+++ attendance/api_views.py
@@ -318,7 +318,9 @@ def api_student_stats(request, username):
     from accounts.models import Subject
     if student.student_class:
         class_subject_ids = Schedule.objects.filter(student_class=student.student_class).values_list('subject_id', flat=True).distinct()
-        class_subjects = Subject.objects.filter(id__in=class_subject_ids)
+        class_subjects = Subject.objects.filter(
+            Q(student_class=student.student_class) | Q(id__in=class_subject_ids)
+        ).distinct()
     else:
         class_subjects = Subject.objects.none()
 
@@ -404,7 +406,10 @@ def api_student_stats(request, username):
             
             sub_verified_od = sub_att.filter(status='OD', date__in=verified_ods).count()
             sub_effective_present = sub_present + sub_verified_od
-            sub_percentage = (sub_effective_present / sub_total * 100) if sub_total > 0 else 0
+            if sub_total > 0:
+                sub_percentage = round((sub_effective_present / sub_total * 100), 2)
+            else:
+                sub_percentage = None
             
             subjects_breakdown.append({
                 'id': sub.id,
@@ -417,7 +422,7 @@ def api_student_stats(request, username):
                 'leave_periods': sub_leave,
                 'verified_od_periods': sub_verified_od,
                 'effective_present': sub_effective_present,
-                'percentage': round(sub_percentage, 2)
+                'percentage': sub_percentage
             })
 
     # AI Suggestion
